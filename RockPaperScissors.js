710bea8511b52e91a5dafc1b61818ea77ffa0c55
KoningHoningh <eelcohoningh@hotmail.com>
2022-05-02 15:02:06 +0200

    added the game to the js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -2,12 +2,43 @@ function computerPlay() {
     const randomValue = Math.floor(Math.random()*3);
     switch (randomValue) {
         case 0:
-            return "Rock"
+            return "ROCK"
             break;
         case 1:
-            return "Paper"
+            return "PAPER"
             break;
         default:
-            return "Scissors"
+            return "SCISSORS"
 }
 };
+function playerPlay() {
+    const playerChoice = prompt("What do you draw?").toUpperCase();
+    console.log(playerChoice);
+    if (playerChoice == "ROCK" || playerChoice == "PAPER" || playerChoice == "SCISSORS")
+    {
+        return playerChoice;
+    }
+    else {playerPlay();}
+}
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return("It's a DRAW!")
+    }
+    else if ((playerChoice == "ROCK" && computerChoice == "SCISSORS") || (playerChoice == "SCISSORS" && computerChoice == "PAPER")||
+     (playerChoice == "PAPER" && computerChoice == "ROCK"))
+    {
+        return 'You WIN! ' + playerChoice + " Beats " + computerChoice + "!";
+    }
+    else {
+        return "You LOSE! " + computerChoice + " Beats " + playerChoice + "!";
+    };
+};
+
+function playGame() {
+    for (let i = 0; i < 5; i++)
+    {
+        console.log(playRound(playerPlay(), computerPlay()));
+    };
+};
+
+playGame();
